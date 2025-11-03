6a4c8844f438a571d27f34b49285085fa9582851
Harsh Raj <harshxroy@gmail.com>
2025-11-03 14:55:17 +0530

    Simplified MongoDb conn

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -4,15 +4,9 @@ const conn = async () => {
   try {
     const mongoURI = process.env.URI;
 
-    if (!mongoURI) {
-      throw new Error("MongoDB URI is missing in .env file");
-    }
+    if (!mongoURI) throw new Error("MongoDB URI is missing in .env file");
 
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
@@ -20,3 +14,4 @@ const conn = async () => {
 };
 
 conn();
+
